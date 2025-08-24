92f52f324996ce36faaa42f00d61f566e0aa4fc7
Hendese Bilgisayar <hendesegurbuz@gmail.com>
2025-08-24 19:59:09 +0300

    done

--- app/src/main/java/com/hendese/widgets/BatteryWidget.kt
+++ app/src/main/java/com/hendese/widgets/BatteryWidget.kt
@@ -2,17 +2,14 @@ package com.hendese.widgets
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.BatteryManager
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
-import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.glance.*
-import androidx.glance.action.ActionParameters
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
-import androidx.glance.appwidget.action.ActionCallback
-import androidx.glance.appwidget.action.actionRunCallback
-import androidx.glance.appwidget.state.updateAppWidgetState
+import androidx.glance.appwidget.provideContent
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
@@ -21,59 +18,33 @@ import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
 
-object CounterWidget: GlanceAppWidget() {
-    val countKey = intPreferencesKey("count")
+object BatteryWidget : GlanceAppWidget() {
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+        provideContent {
+            Content(context = context)
+        }
+    }
 
     @SuppressLint("RestrictedApi")
     @Composable
-    fun Content() {
-        val count = currentState(key = countKey) ?: 0
+    fun Content(context: Context) {
+        val batteryManager = context.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+        val batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+
         Column(
-            modifier = GlanceModifier
-                .fillMaxSize()
-                .background(Color.DarkGray),
+            modifier = GlanceModifier.fillMaxSize().background(Color.DarkGray),
             verticalAlignment = Alignment.Vertical.CenterVertically,
             horizontalAlignment = Alignment.Horizontal.CenterHorizontally
         ) {
             Text(
-                text = count.toString(),
-                style = TextStyle(
-                    fontWeight = FontWeight.Medium,
-                    color = ColorProvider(Color.White),
-                    fontSize = 26.sp
-                )
-            )
-            Button(
-                text = "Inc",
-                onClick = actionRunCallback(IncrementActionCallback::class.java)
+                text = "Pil: $batteryLevel%",
+                style = TextStyle(fontWeight = FontWeight.Medium, color = ColorProvider(Color.White), fontSize = 26.sp)
             )
         }
     }
-
-    override suspend fun provideGlance(context: Context, id: GlanceId) {
-        TODO("Not yet implemented")
-    }
 }
 
-class SimpleCounterWidgetReceiver: GlanceAppWidgetReceiver() {
+class BatteryWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget
-        get() = CounterWidget
-}
-
-class IncrementActionCallback: ActionCallback {
-    override suspend fun onAction(
-        context: Context,
-        glanceId: GlanceId,
-        parameters: ActionParameters
-    ) {
-        updateAppWidgetState(context, glanceId) { prefs ->
-            val currentCount = prefs[CounterWidget.countKey]
-            if(currentCount != null) {
-                prefs[CounterWidget.countKey] = currentCount + 1
-            } else {
-                prefs[CounterWidget.countKey] = 1
-            }
-        }
-        CounterWidget.update(context, glanceId)
-    }
+        get() = BatteryWidget
 }
